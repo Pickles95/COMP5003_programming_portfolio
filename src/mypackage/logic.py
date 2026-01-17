c7fecfc72e8f3404bcd52fdf611f64438e843462
Luke Pickles <L.Pickles.24@unimail.winchester.ac.uk>
2026-01-17 12:04:04 +0000

    Bubble sorting
    Implemented a bubble sorting function and improved clarity of Fibonacci function test

--- src/mypackage/logic.py
+++ src/mypackage/logic.py
@@ -4,9 +4,10 @@
 class FibonacciNth:
 
     def __init__(self):
+         # Begins the list
          self.array = [0, 1]
 
-    def fibonacci(self, n: int):
+    def fibonacci(self, n):
             if n < 0:
                 return "error"
             elif n <= len(self.array):
@@ -20,12 +21,33 @@ class FibonacciNth:
 
 if __name__ == "__main__":
      fibonacci_class = FibonacciNth()
-     user_input = int(input("Please enter your number:"))
-     print(fibonacci_class.fibonacci(user_input))
+     fibonacci_input = int(input("Please enter a number: "))
+     print("The Fibonacci number is: ", fibonacci_class.fibonacci(fibonacci_input))
 
-
+# Adapted from https://www.geeksforgeeks.org/python/python-program-for-n-th-fibonacci-number/
 
 # 3. Sorting (bubble sort) #
+class SortingBubble:
+
+    def bubble_sort(self, array):
+
+        n = len(array)
+
+        for i in range(n):
+            moved = False
+            for j in range (0, n - i - 1):
+                if array[j] > array[j + 1]:
+                    array[j], array[j + 1] = array[j + 1], array[j]
+                    moved = True
+            if (moved == False):
+                break
+        return array
+
+if __name__ == "__main__":
+    sorting_class = SortingBubble()
+    sorting_input = input("Please enter a list of numbers separated by spaces: ")
+    sorted_list = [int(x) for x in sorting_input.split()]
+    print("The sorted list is: ", sorting_class.bubble_sort(sorted_list))
 
 # 4. Brute Force (divide and conquer merge) #
 
